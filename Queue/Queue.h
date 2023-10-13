c466611c948b1cec9e0141ab8ddbe3ab0267e25b
AnnaKasatkina <akasatkina904@gmail.com>
2023-10-13 14:21:52 +0300

    Add base info

--- Queue/Queue.h
+++ Queue/Queue.h
@@ -1,1 +1,21 @@
 #pragma once
+
+typedef struct
+{
+	int front;
+	int back;
+} Queue;
+
+void enqueue();
+
+void dequeue();
+
+void isEmpty();
+
+void deleteQueue();
+
+void front();
+
+void back();
+
+void printQueue();
